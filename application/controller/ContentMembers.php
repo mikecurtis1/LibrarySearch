207b2decba426aac8420250b1647bebf69febfe1
Mike Curtis <mikecurtis1@gmail.com>
2015-11-15 13:36:12 -0500

    Update ContentMembers.php

--- application/controller/ContentMembers.php
+++ application/controller/ContentMembers.php
@@ -1,45 +1,6 @@
 <?php 
 
-class ContentMembers implements Iterator, ContentMembersInterface
+class ContentMembers extends Set
 {
-    private $position = 0;
-    private $members = array();
-    
-    public function current()
-    {
-        return $this->members[$this->position];
-    }
-    
-    public function key()
-    {
-        return $this->position;
-    }
-    
-    public function next()
-    {
-        ++$this->position;
-    }
-    
-    public function rewind()
-    {
-        $this->position = 0;
-    }
-    
-    public function valid()
-    {
-        return isset($this->members[$this->position]);
-    }
-    
-    public function addMember($arg=NULL)
-    {
-        $member_added = false;
-        if (gettype($arg) === 'object') {
-            if (in_array('ContentMemberInterface', class_implements($arg))) {
-                $this->members[] = $arg;
-                $member_added = true;
-            }
-        }
-        
-        return $member_added;
-    }
+
 }
