f63909790b3debeedfd3c9e734891d0db0d2a1ec
Mike Curtis <mikecurtis1@gmail.com>
2016-09-02 15:57:01 -0400

    Update SetFactory.php

--- application/model/SetFactory.php
+++ application/model/SetFactory.php
@@ -5,9 +5,9 @@ class SetFactory extends Set
     public static function build($class='', $member_implementation='')
     {
         if (class_exists($class) && is_string($member_implementation)) {
-			return new $class($member_implementation);
-		} else {
-			return false;
-		}
+		return new $class($member_implementation);
+	} else {
+		return false;
+	}
     }
 }
